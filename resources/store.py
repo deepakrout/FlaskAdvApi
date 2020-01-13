c5ea523daf531031e6f03219516f47fce3806991
Deepak Rout <deepakrout@gmail.com>
2020-01-12 19:22:57 -0500

    Some minor refactor codes

--- resources/store.py
+++ resources/store.py
@@ -11,10 +11,8 @@ class Store(Resource):
 
     def post(self, name):
         if StoreModel.find_by_name(name):
-            return (
-                {"message": "A store with name '{}' already exists.".format(name)},
-                400,
-            )
+            return  {"message": "A store with name '{}' already exists.".format(name)},400,
+            
 
         store = StoreModel(name)
         try:
